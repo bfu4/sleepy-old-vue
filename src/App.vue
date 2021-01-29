ec1bdce00ba0cc2cca90f62a80b26f8d680c8a64
bfu4 <bella@sleeeepyti.me>
2021-01-29 03:20:08 -0500

    hhh hello vercel lol

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div class="m-5 column is-full columns is-centered is-mobile is-multiline">
       <nav-button class="m-1 column is-narrow" img="./images/twitter-grn.png" url="https://twitter.com/bellafusari1"/>
       <nav-button class="m-1 column is-narrow" img="./images/gitgreen.png" url="https://github.com/bfu4"/>
-      <nav-button class="m-1 column is-narrow" img="./images/asu.jpg" round="true" url="https://sleeeepyti.me/extern/catmc.mp4"/>
+      <nav-button class="m-1 column is-narrow" img="./images/asu.jpg" v-bind:round="true" url="https://sleeeepyti.me/extern/catmc.mp4"/>
       <nav-button class="m-1 column is-narrow" img="./images/sc.png" url="https://soundcloud.com/bellafusari"/>
       <nav-button class="m-1 column is-narrow" img="./images/linkedin.png" url="https://www.linkedin.com/in/bella-fusari-2337b3202?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BOvPg3niOTwWUtKyquus9Xg%3D%3D"/>
     </div>
@@ -13,13 +13,14 @@
 </template>
 
 <script lang="ts">
+
 import { Component, Vue } from 'vue-property-decorator';
 import NavButton from "@/components/NavButton.vue";
 
 @Component({
   components: {
     NavButton
-  },
+  }
 })
 export default class App extends Vue {}
 </script>
